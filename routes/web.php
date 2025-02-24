e7cbeed6974913bdd0ef6ed56bf50c82f8387c3e
herw0011 <herw0011@hz.nl>
2025-02-24 10:09:58 +0100

    Fixes the navbar

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+});
+
+Route::get('/faq', function() {
+    return view('faq');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
